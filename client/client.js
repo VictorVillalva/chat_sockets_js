041fbc89fd9b89927ec6d12d675bf3a150290d64
Reus <victor.villalva2003@hotmail.com>
2023-01-28 19:53:12 -0600

    Agregacion de COMENTARIOS

--- client/client.js
+++ client/client.js
@@ -11,14 +11,14 @@ const error = (message) => {
     process.exit(1);
 };
 
-const connect = (host, port) => {
+const connect = (host, port) => { //MUESTRA DESDE DONDE ESTA CONECTADO
     console.log(`Conectado desde --- ${host}:${port}`);
 
     const socket = new Socket();
     socket.connect({ host, port });
     socket.setEncoding("utf-8");
 
-    socket.on("connect", () => {
+    socket.on("connect", () => { //DIGITA EL NOMBRE PARA ENTRAR
         console.log("---------------------------------------------")
         readline.question("Escriba su nombre :  ", (username) => {
             socket.write(username);
@@ -26,24 +26,24 @@ const connect = (host, port) => {
             console.log(`Nota : \n Si desea salir de chat digite  ${END}`);
         });
 
-    readline.on("line", (message) => {
+    readline.on("line", (message) => { //SI ESCRIBE LA CONDICIONAL SALE DEL CHAT
         socket.write("" + message);
         if (message === END) {
             socket.end();
         }
     });
 
-    socket.on("data", (data) => {
+    socket.on("data", (data) => { //MUESTRA LOS MENSAJES 
         console.log(data);
         });
     });
 
-    socket.on("error", (err) => error(err.message));
+    socket.on("error", (err) => error(err.message)); //MANDA MENSAJE DE ERROR
 
-    socket.on("close", () => {
+    socket.on("close", () => {//NOTIFICA CUANDO TERMINAS CONEXION
         console.log("Terminaste Conexion");
         process.exit(0);
     });
 };
 
-connect("127.0.0.1" ,4000);
+connect("127.0.0.1" ,4000); // IP Y PUERTO DE CONEXION
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const serverPort = 4000;
 
 const connections = new Map();
 
-const sendMessage = (message, origin) => {
+const sendMessage = (message, origin) => { //ESTE AYUDA A ENVIAR LOS MENSAJES QUE RECIBE EL SERVER A LOS DEMAS
     for (const socket of connections.keys()) {
         if (socket !== origin) {
             socket.write(message);
@@ -15,12 +15,12 @@ const sendMessage = (message, origin) => {
 };
 
 server.listen(serverPort, () => {
-    console.log("El SuperChat se encuentra el server : ", server.address().port);
+    console.log("El SuperChat se encuentra el puerto : ", server.address().port); //SEÑALA EL PUERTO DE CONEXION
     server.on("connection", (socket) => {
-        const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`;
+        const remoteSocket = `${socket.remoteAddress}:${socket.remotePort}`; //Muestra la direccion junto con el puerto del servidor
         socket.setEncoding('utf-8')
         socket.on("data", (data) => {
-            if ( !connections.has(socket)) {
+            if ( !connections.has(socket)) { //AQUI COMPRUBEA QUE NO SE REPITA EL NOMBRE AL ENTRAR
                 let isRegistered =false;
                 connections.forEach((nickname) => {
                     if (nickname == data) {
@@ -30,27 +30,27 @@ server.listen(serverPort, () => {
                 });
 
             if(!isRegistered) {
-                connections.set(socket, data);
+                connections.set(socket, data); //AQUI MUESTRA EL NOMBRE CON EL CUAL SE REGISTRO Y LE MANDO UN MENSAJE QUE SE ENCUENTRA EN LINEA
                 console.log(remoteSocket, " se conecto con el nombre de ---> ", data);
                 socket.write('********ESTADO: EN LINEA********');
                 const userConnected = data;
-                sendMessage(userConnected + " se conecto")
+                sendMessage(userConnected + " se conecto") //ENVIA A LAS DEMAS PERSONAS CONECTADAS, QUE SE ACABA DE CONECTAR
             }
             }else {
                 console.log('------------------------------------------')
-                console.log(`|${connections.get(socket)}| : ${data}`);
+                console.log(`|${connections.get(socket)}| : ${data}`); //Muestra los mensajes enviados en el server
                 console.log('------------------------------------------')
-                const fullMessage = `[${connections.get(socket)}]: ${data}`;
+                const fullMessage = `[${connections.get(socket)}]: ${data}`; //MUESTRA EL MENSAJE A LOS DEMAS CONECTADOS
                 console.log(`${remoteSocket} -> ${fullMessage}`);
                 sendMessage(fullMessage, socket);
             }
         });
-        socket.on("error", (err) => {
+        socket.on("error", (err) => { //MUESTRA ALGUN ERROR 
             console.error(err.message);
             process.exit(1);
         });
     
-        socket.on("close", () => {
+        socket.on("close", () => { //AQUI NOTIFICA SI EXISTE ALGUNA DESCONEXION DEL CHAT
             console.log(connections.get(socket), " Finalizo la Comunicacion");
             const disconnectedUser = `${connections.get(socket)}`;
             sendMessage(disconnectedUser +" se desconecto");
